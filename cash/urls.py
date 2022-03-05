0b8abb7a98cf6f0beb0ec71d489690d5aa627e95
jack <tjdgns526@gmail.com>
2022-03-05 23:27:35 +0900

    질문 상세 페이지 수정

---
+++ cash/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path
+
+app_name = 'cash'
+
+urlpatterns = [
+    path('', views.index, name = 'index'),
+    path('<int:question_id>/', views.detail, name = 'detail'), 
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    path('question/create/', views.question_create, name='question_create'),
+]
--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from cash import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sign/', include('sign.urls')),
+    path('cash/', include('cash.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),
 ]
